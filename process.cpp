cab35b73fa0e7e66bdd0daf16135b5a4f5278efc
linus <2241029754liu@gmail.com>
2025-08-10 11:12:01 +0800

    version 0.0.1

---
+++ process.cpp
@@ -0,0 +1,9 @@
+#include"process.h"
+#include<iostream>
+using namespace std;
+void Process::planProcess()
+{
+    cout<<"this is planing process"<<endl;
+    my_map.mapInfo();
+    cout<<"planing success"<<endl;
+}
